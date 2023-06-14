62a64113bb7b5e06bf8243f7de6e8a792d7d2336
SIJAN <73060875+SIJAN61@users.noreply.github.com>
2023-06-14 16:10:45 +0600

    Add files via upload

---
+++ Task 1/index.js
@@ -0,0 +1,17 @@
+//1.Find all the students from science section
+const students = [
+    { name: 'Quincy', grade: 96, gendar: 'Female', hasGoodGrade: true, section: 'Science', age: 21 },
+    { name: 'Jason', grade: 84, gendar: 'Male', hasGoodGrade: true, section: 'Arts', age: 18 },
+    { name: 'Alexis', grade: 100, gendar: 'Female', hasGoodGrade: true, section: 'Science', age: 22 },
+    { name: 'Sam', grade: 65, gendar: 'male', hasGoodGrade: false, section: 'Commerce', age: 19 },
+    { name: 'Katie', grade: 90, gendar: 'Female', hasGoodGrade: true, section: 'Commerce', age: 17  }
+  ];
+
+
+  //function Science(student){
+        // return student.section==='Science' }
+ 
+ // console.log(students.find(({section}) => section==="Science" ))
+
+const scienceStudents = students.filter(student => student.section==="Science");
+console.log(scienceStudents)
